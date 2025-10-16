9b8f67c072e94b85d96368c992dc49f4eb6f1e34
juhi0589 <58846047+juhi0589@users.noreply.github.com>
2025-10-17 02:06:36 +0530

    Rename S3 bucket for Terraform state

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-buck"
+  bucket = "demo-terraform-eks-state-s3-bucket53"
 
   lifecycle {
     prevent_destroy = false
